08934dc6f1356dc623a603e2cdbe6a4b5e37dfe3
Flynt Gao <flynt.gao@consultant.udtrucks.com>
2024-05-16 13:41:53 +0800

    change all field and list

--- src/webparts/bsipoc/components/App.tsx
+++ src/webparts/bsipoc/components/App.tsx
@@ -8,10 +8,11 @@ import "@pnp/sp/folders";
 import { PrimaryButton, Stack } from "office-ui-fabric-react";
 import * as XLSX from 'xlsx';
 import { Dropdown, IDropdownStyles, IDropdownOption } from '@fluentui/react/lib/Dropdown';
-import ReadExcelFromSP from "./ReadExcelFromSp";
+
 import YearPicker from "./control/YearSelect";
 import createrFolder from "./control/CreateFolder"
 import * as Excel from 'exceljs';
+import AppContext from "../../../common/AppContext";
 // import XlxsExcelFromSP from "./xlxsexcel"
 
 
@@ -30,9 +31,12 @@ const options: IDropdownOption[] = [
 
 export default memo(function App() {
   const sp = spfi(getSP());
-
-
-
+  const ctx = React.useContext(AppContext);
+  const webURL = ctx.context?._pageContext?._web?.absoluteUrl;
+  const userEmail = ctx.context?._pageContext?._user?.email;
+  const Site_Relative_Links = webURL.slice(webURL.indexOf('/sites'))
+  console.log("site",Site_Relative_Links)
+  console.log("weburl",webURL,userEmail)
   const [excel, setExcel] = React.useState([]);
   const [allCountry, setAllCountry] = React.useState([])
   const [priceTable, setPrice] = React.useState({})
@@ -56,36 +60,39 @@ export default memo(function App() {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return orders.map((val: any) => {
       const obj: any = {
-        Country: val.field_2,
-        Dealer: val.field_3,
-        "Dealer ID": val.field_4,
-        "Dealer Type": val.field_6,
-        "Dealer Category": val.field_8,
-        "Basic Package": val.field_9,
-        "Sales Package": val.field_10,
-        "CPQ": val.field_11,
-        "UD CM": val.field_12,
-        "Argus 365": val.field_13,
-        "UDCP": val.field_14,
-        "SeMA": val.field_15,
-        "LDS": val.field_16,
-        "LSS": val.field_17,
-        "Pardot": val.field_18
+        Market: val.Market,
+        Dealer: val.PartnerName,
+        "Dealer ID": val.PartnerID,
+        "Dealer Type": val.DealerType,
+        "Dealer Category": val.DealerCategory,
+        "Basic Package": val.BasicPackage,
+        "Sales Package": val.SalesPackage,
+        "Hub Package":val.HubPackage,
+        "CPQ": val.CPQ,
+        "UD CM": val.UDCM,
+        "Argus 365": val.Argus365,
+        "UDCP": val.UDCP,
+        "SeMA": val.SeMA,
+        "LDS": val.LDS,
+        "LSS": val.LSS,
+        "Pardot": val.Pardot,
+        
       }
-
-      const keys = ['CPQ', 'UD CM', 'Argus 365', 'UDCP', 'SeMA', 'LDS', 'LSS', 'Pardot']
+      // console.log("obj",obj)
+      const keys = ['Hub Package','CPQ', 'UD CM', 'Argus 365', 'UDCP', 'SeMA', 'LDS', 'LSS', 'Pardot']
       obj['Total(Per Month)'] = keys.reduce((sum, key) => sum + (priceMap[key] || 0) * obj[key], 0)
-      obj['Total(Per Month)'] += (priceMap['Basic Package;' + val.field_8] || 0) * obj['Basic Package']
-      obj['Total(Per Month)'] += (priceMap['Sales Package;' + (val.field_8 || 'NA')] || 0) * obj['Sales Package']
+      obj['Total(Per Month)'] += (priceMap['Basic Package;' + val.DealerCategory] || 0) * obj['Basic Package']
+      obj['Total(Per Month)'] += (priceMap['Sales Package;' + (val.DealerCategory || 'NA')] || 0) * obj['Sales Package']
       return obj
     })
   }
 
   function calcToSummary(details: any, priceMap: any) {
     const resObj: any = {
-      country: details[0].Country,
+      Country: details[0].Market,
       data: []
     }
+    console.log("res",resObj)
     const p = {...priceMap}
     for(let key in p) {
       const value = p[key]
@@ -125,7 +132,7 @@ export default memo(function App() {
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   const initData = async () => {
     // 拿应用价格表
-    const appObj = await sp.web.lists.getByTitle("UD BSI_AppPriceMaster").renderListDataAsStream({
+    const appObj = await sp.web.lists.getByTitle("ApplicationPriceMaster").renderListDataAsStream({
       /* 字段关系如下
       Title ===Application Name
       field_1 = Price Type
@@ -135,9 +142,9 @@ export default memo(function App() {
 
       ViewXml: `<View>
                       <ViewFields>
-                        <FieldRef Name="Title"/>
-                        <FieldRef Name="field_1"/>
-                        <FieldRef Name="field_2"/>
+                        <FieldRef Name="ApplicationName"/>
+                        <FieldRef Name="PriceType"/>
+                        <FieldRef Name="Price_x0028_USD_x0029_"/>
                       </ViewFields>
                    
                     </View>`,
@@ -148,7 +155,7 @@ export default memo(function App() {
       if (response.Row.length > 0) {
         const resObj: any = {}
         response.Row.forEach(val => {
-          resObj[val.Title] = val.field_2 * 1
+          resObj[val.ApplicationName] = val.Price_x0028_USD_x0029_ * 1
         })
         return resObj
       }
@@ -158,7 +165,7 @@ export default memo(function App() {
 
 
     // 拿包的单价表
-    const packageObj = await sp.web.lists.getByTitle("UD BSI_PackageMaster").renderListDataAsStream({
+    const packageObj = await sp.web.lists.getByTitle("PackageMaster").renderListDataAsStream({
       /* 字段关系如下
       Title ===Package Name
       field_1 = PartnerType
@@ -169,10 +176,10 @@ export default memo(function App() {
 
       ViewXml: `<View>
                       <ViewFields>
-                        <FieldRef Name="Title"/>
-                        <FieldRef Name="field_1"/>
-                        <FieldRef Name="field_2"/>
-                        <FieldRef Name="field_3"/>
+                        <FieldRef Name="PackageCategory"/>
+                        <FieldRef Name="PartnerType"/>
+                        <FieldRef Name="DealerCategory"/>
+                        <FieldRef Name="MonthlyPrice_x0028_USD_x0029_"/>
                         <FieldRef Name="Comment"/>
                       </ViewFields>
                    
@@ -184,8 +191,9 @@ export default memo(function App() {
       if (response.Row.length > 0) {
         const resObj: any = {}
         response.Row.forEach(val => {
-          resObj[`${val.Title};${val.field_2}`] = val.field_3 * 1
+          resObj[`${val.PackageCategory};${val.DealerCategory}`] = val.MonthlyPrice_x0028_USD_x0029_ * 1
         })
+        console.log("resobji",resObj)
         return resObj
       }
 
@@ -193,19 +201,15 @@ export default memo(function App() {
     })
 
     // 拿period 周期对应关系
-    const period = await sp.web.lists.getByTitle("BSI_Period").renderListDataAsStream({
-      /* 字段关系如下
-      Title ===Package Name
-      field_1 = PartnerType
-      field_2 = Dealer Category
-      field_3 = Monthly Price (USD)
-      Comment = Comment
-      */
+    const period = await sp.web.lists.getByTitle("Cost Calculation Period").renderListDataAsStream({
 
       ViewXml: `<View>
                       <ViewFields>
-                        <FieldRef Name="Title"/>
-                        <FieldRef Name="Month"/>
+                        <FieldRef Name="PeriodName"/>
+                        <FieldRef Name="PeriodDetails"/>
+                        <FieldRef Name="NumberofMonths"/>
+                        <FieldRef Name="AvailableforSelection"/>
+                        <FieldRef Name="Generated"/>
                       </ViewFields>
                    
                     </View>`,
@@ -225,7 +229,7 @@ export default memo(function App() {
     })
 
     // 拿主表订单
-    const order = await sp.web.lists.getByTitle("UD BSI_PartnerConfig").renderListDataAsStream({
+    const order = await sp.web.lists.getByTitle("Partner Master").renderListDataAsStream({
       /* 字段关系如下
       Title ===Hub
       field_1 = PartnerType
@@ -251,26 +255,27 @@ export default memo(function App() {
 
       ViewXml: `<View>
                       <ViewFields>
-                        <FieldRef Name="Title"/>
-                        <FieldRef Name="field_1"/>
-                        <FieldRef Name="field_2"/>
-                        <FieldRef Name="field_3"/>
-                        <FieldRef Name="field_4"/>
-                        <FieldRef Name="field_5"/>
-                        <FieldRef Name="field_6"/>
-                        <FieldRef Name="field_7"/>
-                        <FieldRef Name="field_8"/>
-                        <FieldRef Name="field_9"/>
-                        <FieldRef Name="field_10"/>
-                        <FieldRef Name="field_11"/>
-                        <FieldRef Name="field_12"/>
-                        <FieldRef Name="field_13"/>
-                        <FieldRef Name="field_14"/>
-                        <FieldRef Name="field_15"/>
-                        <FieldRef Name="field_16"/>
-                        <FieldRef Name="field_17"/>
-                        <FieldRef Name="field_18"/>
-                        <FieldRef Name="field_19"/>
+                        <FieldRef Name="Hub"/>
+                        <FieldRef Name="PartnerType"/>
+                        <FieldRef Name="Market"/>
+                        <FieldRef Name="PartnerName"/>
+                        <FieldRef Name="PartnerID"/>
+                        <FieldRef Name="ExclusiveorMultibrand"/>
+                        <FieldRef Name="DealerType"/>
+                        <FieldRef Name="NoofBays"/>
+                        <FieldRef Name="DealerCategory"/>
+                        <FieldRef Name="BasicPackage"/>
+                        <FieldRef Name="SalesPackage"/>
+                        <FieldRef Name="HubPackage"/>
+                        <FieldRef Name="CPQ"/>
+                        <FieldRef Name="UDCM"/>
+                        <FieldRef Name="Argus365"/>
+                        <FieldRef Name="UDCP"/>
+                        <FieldRef Name="SeMA"/>
+                        <FieldRef Name="LDS"/>
+                        <FieldRef Name="LSS"/>
+                        <FieldRef Name="Pardot"/>
+                        
                         
                       </ViewFields>
                    
@@ -280,7 +285,7 @@ export default memo(function App() {
       console.log("主表订单", response.Row)
       // console.log("res", response.Row.filter((item)=>item.field_2==="NZ"))
       if (response.Row.length > 0) {
-        const uniqueList = Array.from(new Set(response.Row.map(item => item.field_2)))
+        const uniqueList = Array.from(new Set(response.Row.map(item => item.Market)))
         console.log("country", uniqueList)
         setAllCountry(uniqueList)
         //setAllCountry
@@ -296,6 +301,7 @@ export default memo(function App() {
     setPrice(price)
     const finalExcelData = calcToExcel(order, price)
     console.log(calcToSummary(finalExcelData, price))
+    console.log("excel121",finalExcelData)
     setExcel(finalExcelData)
   }
 
@@ -313,7 +319,7 @@ export default memo(function App() {
     })
 
     const worksheetDetail = workbook.getWorksheet(2)
-    const zimu = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'M', 'O', 'P']
+    const zimu = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'M', 'O', 'P',"Q"]
     zimu.forEach(z => {
       worksheetDetail.getCell(z + '2').fill= {
         type: 'pattern',
@@ -350,15 +356,15 @@ export default memo(function App() {
     // 创建一个数组来存储所有的上传Promise
     const uploadPromises: any[] = [];
 
-    const buffer = await sp.web.getFileByServerRelativePath("/sites/proj-testspfeatures/Shared Documents/UD BSI_Output Template.xlsx").getBuffer();
-
+    const buffer = await sp.web.getFileByServerRelativePath(Site_Relative_Links+"/BSITemplate/UD BSI_Output Template.xlsx").getBuffer();
 
     // 遍历所有国家
-    allCountry.forEach(country => {
+    allCountry.forEach(Market => {
       // 筛选出该国家的订单
-      const countryOrders = excel.filter(order => order.Country === country);
+      // console.log("2exe",excel)
+      const countryOrders = excel.filter(order => order.Market === Market);
       if (countryOrders.length === 0) {
-        console.log(`No data for ${country}`);
+        console.log(`No data for ${Market}`);
         return;
       }
 
@@ -369,8 +375,8 @@ export default memo(function App() {
       // const arrTpt = XLSX.utils.sheet_to_json(workSheetSummaryTpt)
       
       const tongji = calcToSummary(countryOrders, priceTable)
-      console.log(tongji)
-      workSheetSummaryTpt['B2'] = { v: tongji.country }
+      // \console.log("tongji",tongji)
+      workSheetSummaryTpt['B2'] = { v: tongji.Market }
       workSheetSummaryTpt['E2'] = { v: `${selectedYear}/${(Number(selectedKey.replace('Q', '')) -1 )*3+1} - ${selectedYear}/${(Number(selectedKey.replace('Q', '')))*3}` }
       for(let i = 5; i<tongji.data.length+5; i++) {
         workSheetSummaryTpt['A'+ i] = { v: tongji.data[i-5].A}
@@ -395,7 +401,7 @@ export default memo(function App() {
       // });
       const summaryTemplateName2 = workbookTemplate.SheetNames[3]
       const workSheetDetails = workbookTemplate.Sheets[summaryTemplateName2]
-      const zimu = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'M', 'O', 'P']
+      const zimu = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q']
       for(let i = 3; i<countryOrders.length+3; i++) {
         let j = 0;
         for(let key in countryOrders[i-3]) {
@@ -403,8 +409,8 @@ export default memo(function App() {
           j++
         }
       }
-      workSheetDetails['!ref'] = 'A1:P100'
-
+      workSheetDetails['!ref'] = 'A1:R100'
+      
       // 创建工作簿
       const workbook = XLSX.utils.book_new();
       XLSX.utils.book_append_sheet(workbook, workSheetSummaryTpt, "Market Summary");
@@ -414,18 +420,18 @@ export default memo(function App() {
       // 将工作簿转换为Blob
       const wbout = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
       // const blob = new Blob([wbout], { type: "application/octet-stream" });
-
+      
       changeStyle(wbout).then(blob => {
         // 添加上传任务到数组
         const uploadPromise = uploadFileToSP(
-          `/sites/proj-testspfeatures/Shared Documents/${selectedYear}${selectedKey}`,
-          `UD ${country} ${selectedYear}${selectedKey}.xlsx`,
+          `${Site_Relative_Links}/Shared Documents/${selectedYear}${selectedKey}`,
+          `UD ${Market} ${selectedYear}${selectedKey}.xlsx`,
           blob
         );
         uploadPromises.push(uploadPromise);
       })
     });
-    await createrFolder("/sites/proj-testspfeatures/Shared Documents/", selectedYear+selectedKey)
+    // await createrFolder(Site_Relative_Links+"/Shared Documents/", selectedYear+selectedKey)
     // 等待所有文件上传完成
     Promise.all(uploadPromises).then(() => {
       alert("All cost summaries are generated and uploaded successfully.");
@@ -451,8 +457,9 @@ export default memo(function App() {
       return;
   }
     try {
-        await createrFolder("/sites/proj-testspfeatures/Shared Documents", selectedYear+selectedKey);
+        await createrFolder(Site_Relative_Links+"/Shared Documents", selectedYear+selectedKey);
         alert("文件夹创建成功!");
+        await handleExport();
     } catch (error) {
         alert("创建文件夹失败: " + error.message);
     }
@@ -473,10 +480,10 @@ export default memo(function App() {
           onChange={onChange} // 绑定onChange事件处理器
           selectedKey={selectedKey} // 设置选中项
         />
-        <PrimaryButton style={{ marginTop: 10 }} disabled={excel.length === 0 || selectedKey === ''} onClick={handleExport}>generate excel file</PrimaryButton>
+        <PrimaryButton style={{ marginTop: 10 }} disabled={excel.length === 0 || selectedKey === ''} onClick={handleCreateFolder}>generate excel file</PrimaryButton>
       </Stack>
 <Stack>
-      <ReadExcelFromSP />
+      {/* <ReadExcelFromSP /> */}
       <div>分割</div>
       <PrimaryButton onClick={handleCreateFolder}/>
       </Stack>
